e75319ba1c429dcb042d19c2a7ec991ad76a4e5d
Asaf Bartov <asaf.bartov@gmail.com>
2025-11-26 21:11:34 +0200

    Upgrade to Rails 7.x

--- Gemfile
+++ Gemfile
@@ -1,34 +1,33 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.3'
+gem 'rails', '~>7.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem 'mysql2'
-gem 'openlibrary'
 gem 'haml-rails'
-gem 'thin'
+gem 'puma'
+gem 'logger'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails'
 gem 'bootstrap-sass'
-
-gem 'app_constants' # for configuration globals
+gem 'rest-client'
 gem 'httparty' # for consuming JSON APIs
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-gem 'therubyracer', platforms: :ruby
+gem 'mini_racer'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
+gem 'jbuilder'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'sdoc', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -41,13 +40,14 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  gem 'debug'
   gem 'cucumber'
   gem 'cucumber-rails', require: false
   gem 'database_cleaner'
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-
+  gem 'web-console'
+  gem 'rspec'
+  gem 'rspec-rails'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,169 +1,155 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722063655) do
-
+ActiveRecord::Schema[7.2].define(version: 2025_11_26_033122) do
   create_table "embodiments", force: :cascade do |t|
-    t.integer  "expression_id"
-    t.integer  "manifestation_id"
-    t.string   "reltype"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "sequence_number"
-  end
-
-  add_index "embodiments", ["expression_id"], name: "index_embodiments_on_expression_id"
-  add_index "embodiments", ["manifestation_id"], name: "index_embodiments_on_manifestation_id"
-
-  create_table "expression_relationships", force: :cascade do |t|
-    t.integer  "exp1_id"
-    t.integer  "exp2_id"
-    t.string   "reltype"
-    t.integer  "creator_id"
-    t.integer  "reviewer_id"
-    t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "expression_relationships", ["creator_id"], name: "index_expression_relationships_on_creator_id"
-  add_index "expression_relationships", ["exp1_id"], name: "index_expression_relationships_on_exp1_id"
-  add_index "expression_relationships", ["exp2_id"], name: "index_expression_relationships_on_exp2_id"
-  add_index "expression_relationships", ["reviewer_id"], name: "index_expression_relationships_on_reviewer_id"
-
-  create_table "expressions", force: :cascade do |t|
-    t.string   "title"
-    t.string   "publication_date"
-    t.string   "creation_date"
-    t.string   "language"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "expressions", ["language"], name: "index_expressions_on_language"
-  add_index "expressions", ["title"], name: "index_expressions_on_title"
-
-  create_table "manifestations", force: :cascade do |t|
-    t.string   "title"
-    t.string   "responsibility"
-    t.string   "edition"
-    t.string   "publisher"
-    t.string   "publication_date"
-    t.string   "publication_place"
-    t.string   "series_statement"
-    t.text     "comment"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-  end
-
-  add_index "manifestations", ["title"], name: "index_manifestations_on_title"
-
-  create_table "people", force: :cascade do |t|
-    t.string   "name"
-    t.string   "dates"
-    t.string   "title"
-    t.string   "affiliation"
-    t.string   "country"
-    t.text     "comment"
-    t.integer  "viaf_id"
-    t.integer  "wikidata_q"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "openlibrary_id"
-  end
-
-  add_index "people", ["openlibrary_id"], name: "index_people_on_openlibrary_id"
-  add_index "people", ["viaf_id"], name: "index_people_on_viaf_id"
-  add_index "people", ["wikidata_q"], name: "index_people_on_wikidata_q"
-
-  create_table "people_works", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "work_id"
+    t.integer "expression_id"
+    t.integer "manifestation_id"
+    t.string "reltype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sequence_number"
+    t.index ["expression_id"], name: "index_embodiments_on_expression_id"
+    t.index ["manifestation_id"], name: "index_embodiments_on_manifestation_id"
   end
 
-  add_index "people_works", ["person_id"], name: "index_people_works_on_person_id"
-  add_index "people_works", ["work_id"], name: "index_people_works_on_work_id"
+  create_table "expression_relationships", force: :cascade do |t|
+    t.integer "exp1_id"
+    t.integer "exp2_id"
+    t.string "reltype"
+    t.integer "creator_id"
+    t.integer "reviewer_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_expression_relationships_on_creator_id"
+    t.index ["exp1_id"], name: "index_expression_relationships_on_exp1_id"
+    t.index ["exp2_id"], name: "index_expression_relationships_on_exp2_id"
+    t.index ["reviewer_id"], name: "index_expression_relationships_on_reviewer_id"
+  end
+
+  create_table "expressions", force: :cascade do |t|
+    t.string "title"
+    t.string "publication_date"
+    t.string "creation_date"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language"], name: "index_expressions_on_language"
+    t.index ["title"], name: "index_expressions_on_title"
+  end
+
+  create_table "manifestations", force: :cascade do |t|
+    t.string "title"
+    t.string "responsibility"
+    t.string "edition"
+    t.string "publisher"
+    t.string "publication_date"
+    t.string "publication_place"
+    t.string "series_statement"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_manifestations_on_title"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "dates"
+    t.string "title"
+    t.string "affiliation"
+    t.string "country"
+    t.text "comment"
+    t.integer "viaf_id"
+    t.integer "wikidata_q"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "openlibrary_id"
+    t.index ["openlibrary_id"], name: "index_people_on_openlibrary_id"
+    t.index ["viaf_id"], name: "index_people_on_viaf_id"
+    t.index ["wikidata_q"], name: "index_people_on_wikidata_q"
+  end
+
+  create_table "people_works", force: :cascade do |t|
+    t.integer "person_id"
+    t.integer "work_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_people_works_on_person_id"
+    t.index ["work_id"], name: "index_people_works_on_work_id"
+  end
 
   create_table "realizations", force: :cascade do |t|
-    t.integer  "realizer_id"
-    t.integer  "expression_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "realizer_id"
+    t.integer "expression_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expression_id"], name: "index_realizations_on_expression_id"
+    t.index ["realizer_id"], name: "index_realizations_on_realizer_id"
   end
-
-  add_index "realizations", ["expression_id"], name: "index_realizations_on_expression_id"
-  add_index "realizations", ["realizer_id"], name: "index_realizations_on_realizer_id"
 
   create_table "reifications", force: :cascade do |t|
-    t.integer  "work_id"
-    t.integer  "expression_id"
-    t.string   "reltype"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "work_id"
+    t.integer "expression_id"
+    t.string "reltype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expression_id"], name: "index_reifications_on_expression_id"
+    t.index ["work_id"], name: "index_reifications_on_work_id"
   end
-
-  add_index "reifications", ["expression_id"], name: "index_reifications_on_expression_id"
-  add_index "reifications", ["work_id"], name: "index_reifications_on_work_id"
 
   create_table "tocs", force: :cascade do |t|
-    t.string   "book_uri"
-    t.text     "toc_body"
-    t.string   "status"
-    t.integer  "contributor_id"
-    t.integer  "reviewer_id"
-    t.text     "comments"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "manifestation_id"
-    t.string   "title"
+    t.string "book_uri"
+    t.text "toc_body"
+    t.string "status"
+    t.integer "contributor_id"
+    t.integer "reviewer_id"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "manifestation_id"
+    t.string "title"
+    t.index ["book_uri"], name: "index_tocs_on_book_uri", unique: true
+    t.index ["contributor_id"], name: "index_tocs_on_contributor_id"
+    t.index ["manifestation_id"], name: "index_tocs_on_manifestation_id"
+    t.index ["reviewer_id"], name: "index_tocs_on_reviewer_id"
   end
-
-  add_index "tocs", ["book_uri"], name: "index_tocs_on_book_uri", unique: true
-  add_index "tocs", ["contributor_id"], name: "index_tocs_on_contributor_id"
-  add_index "tocs", ["manifestation_id"], name: "index_tocs_on_manifestation_id"
-  add_index "tocs", ["reviewer_id"], name: "index_tocs_on_reviewer_id"
 
   create_table "work_relationships", force: :cascade do |t|
-    t.integer  "work1_id"
-    t.integer  "work2_id"
-    t.string   "reltype"
-    t.integer  "creator_id"
-    t.integer  "reviewer_id"
-    t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "work1_id"
+    t.integer "work2_id"
+    t.string "reltype"
+    t.integer "creator_id"
+    t.integer "reviewer_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_work_relationships_on_creator_id"
+    t.index ["reviewer_id"], name: "index_work_relationships_on_reviewer_id"
+    t.index ["work1_id"], name: "index_work_relationships_on_work1_id"
+    t.index ["work2_id"], name: "index_work_relationships_on_work2_id"
   end
-
-  add_index "work_relationships", ["creator_id"], name: "index_work_relationships_on_creator_id"
-  add_index "work_relationships", ["reviewer_id"], name: "index_work_relationships_on_reviewer_id"
-  add_index "work_relationships", ["work1_id"], name: "index_work_relationships_on_work1_id"
-  add_index "work_relationships", ["work2_id"], name: "index_work_relationships_on_work2_id"
 
   create_table "works", force: :cascade do |t|
-    t.string   "title"
-    t.string   "form"
-    t.string   "creation_date"
-    t.text     "comment"
-    t.string   "status"
-    t.integer  "superseded_by"
-    t.integer  "wikidata_q"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "title"
+    t.string "form"
+    t.string "creation_date"
+    t.text "comment"
+    t.string "status"
+    t.integer "superseded_by"
+    t.integer "wikidata_q"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_works_on_title"
+    t.index ["wikidata_q"], name: "index_works_on_wikidata_q"
   end
-
-  add_index "works", ["title"], name: "index_works_on_title"
-  add_index "works", ["wikidata_q"], name: "index_works_on_wikidata_q"
-
 end
